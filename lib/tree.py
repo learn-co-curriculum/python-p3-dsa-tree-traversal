241266b0fc71f8fff0ee013070e2eda1e3305911
Ben Botsford <ben.botsford@flatironschool.com>
2023-01-24 14:59:03 -0600

    add solution back in

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,12 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+
+    while nodes_to_visit:
+      current = nodes_to_visit.pop()
+      if current['id'] == id:
+        return current
+
+      nodes_to_visit = nodes_to_visit + current['children']
+    return None
